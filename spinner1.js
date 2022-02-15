b44e3f5827bd466be1e72943d27e65bbc699a84b
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-15 16:07:30 -0700

    add spinner animation

---
+++ spinner1.js
@@ -0,0 +1,37 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 400);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2000);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 2300);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 2700);
